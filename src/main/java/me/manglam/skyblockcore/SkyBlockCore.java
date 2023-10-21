934f84a9e76d69ca440238e9e742b0be55f6c8db
Legacity <131291817+Legacity@users.noreply.github.com>
2023-10-21 00:56:25 -0700

    idk, alot of things

--- src/main/java/me/manglam/skyblockcore/SkyBlockCore.java
+++ src/main/java/me/manglam/skyblockcore/SkyBlockCore.java
@@ -1,17 +1,32 @@
 package me.manglam.skyblockcore;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import me.manglam.skyblockcore.Player.CustomScoreboard;
+import me.manglam.skyblockcore.core.Stats;
+import me.manglam.skyblockcore.core.Commands.CommandsManager;
 
 public final class SkyBlockCore extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        setupEnums();
+        registerCommands();
+        registerListeners();
 
+        CustomScoreboard customScoreboard = new CustomScoreboard(this);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+    }
+
+    private void setupEnums() {
+    }
+
+    private void registerCommands() {
+        getCommand("skyblock").setExecutor(new CommandsManager());
+    }
+
+    private void registerListeners() {
     }
 }
