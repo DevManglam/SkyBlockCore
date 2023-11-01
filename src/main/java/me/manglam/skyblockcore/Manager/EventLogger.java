4a6b2bd112c1bdf254bf8801226af4eb9a70bda6
ProxyD_ <87957190+Dctrrr@users.noreply.github.com>
2023-11-01 11:22:51 +0530

    Create EventLogger.java

---
+++ src/main/java/me/manglam/skyblockcore/Manager/EventLogger.java
@@ -0,0 +1,24 @@
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EventLogger extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+    }
+
+    @EventHandler
+    public void onPlayerLeave(PlayerQuitEvent event) {
+    }
+}
+
+// Yeah this is just a test code
+// Will use this to log events such as player command execution etc
